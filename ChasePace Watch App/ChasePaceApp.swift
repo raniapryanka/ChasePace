91b908ee07525843fc0f4e8ea13791c9787c4193
pippy pryanka <pippypryanka3@gmail.com>
2024-05-21 14:02:11 +0700

    Initial Commit

---
+++ ChasePace Watch App/ChasePaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChasePaceApp.swift
+//  ChasePace Watch App
+//
+//  Created by Rania Pryanka Arazi on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChasePace_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChasePace Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChasePace Watch App
+//
+//  Created by Rania Pryanka Arazi on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
